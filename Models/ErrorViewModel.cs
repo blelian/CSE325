1516d69fe3090c49d567820c8ce7e2899fdb8d5e
blelian <bullsurus@gmail.com>
2025-11-07 04:54:58 +0200

    Initial commit - BlessingMovies MVC project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BlessingMovies.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
